5e98eb4935eee8e2cf8b9c80209a28bc423a0f96
alessandrodiroberto <alessandro.diroberto@intecs.it>
2024-01-31 15:24:30 +0100

    Add html page

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,16 @@
+"use strict";
 //PROMISE RESOLVE
 //Con resolve, viene ritornato un valore
 let p = Promise.resolve('Promise resolved');
-
-p.then((response)=> {
+p.then((response) => {
     console.log(response);
 });
-
 //PROMISE REJECT
 let r = Promise.reject('Promise not resolved');
 r.then(promiseResolved).catch(promiseNotResolved);
-
-function promiseResolved(v : any){
+function promiseResolved(v) {
     console.log(v);
 }
-
-function promiseNotResolved(err : any){
+function promiseNotResolved(err) {
     console.log(err);
 }
