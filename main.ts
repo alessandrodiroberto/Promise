f738d97e24b5387581b2588b81a696667fc2c00a
alessandrodiroberto <alessandro.diroberto@intecs.it>
2024-01-31 15:03:35 +0100

    First

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-//PROMISE
+//PROMISE RESOLVE
+//Con resolve, viene ritornato un valore
+let p = Promise.resolve('Promise resolved');
+
+p.then((response)=> {
+    console.log(response);
+});
+
+
+//PROMISE REJECT
+let r = Promise.reject('Promise not resolved');
+r.then(promiseResolved).catch(promiseNotResolved);
+
+function promiseResolved(v : any){
+    console.log(v);
+}
+
+function promiseNotResolved(err : any){
+    console.log(err);
+}
